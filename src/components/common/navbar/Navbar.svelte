3a0a653f0a3ceeca31b39269fee1ea5a774d1281
Dinh-Van Colomban <van.colomban@gmail.com>
2023-08-15 20:01:47 +0200

    feat(navbar): initial styling

--- src/components/common/navbar/Navbar.svelte
+++ src/components/common/navbar/Navbar.svelte
@@ -1,6 +1,9 @@
 <script lang="ts">
   import { onDestroy, onMount } from 'svelte';
 
+  import { writable } from 'svelte/store';
+
+  import { inView } from '~/actions';
   import { HeaderLink } from '~/data';
   import { scrollToHash, scrollToUrkHash, useHashAnchors } from '~/utils/router.utils';
 
@@ -11,9 +14,19 @@
     timeout = setTimeout(scrollToUrkHash, 100);
   });
   onDestroy(() => clearTimeout(timeout));
+
+  const visible$ = writable(false);
 </script>
 
-<nav id="{HeaderLink.Home}}" class="hero-nav">
+<nav
+  id="{HeaderLink.Home}}"
+  class="hero-nav"
+  class:visible={$visible$}
+  use:inView={{ margin: { bottom: 200 } }}
+  on:enter={() => {
+    $visible$ = true;
+  }}
+>
   <ul>
     <li><a on:click={() => scrollToHash(HeaderLink.Projects)} href={null}>Projects</a></li>
     <li><a on:click={() => scrollToHash(HeaderLink.Skills)} href={null}>Skills</a></li>
@@ -23,8 +36,17 @@
 </nav>
 
 <style lang="scss">
-  nav {
+  @use 'src/theme/colors';
+
+  .hero-nav {
     padding: 2rem;
+    opacity: 0;
+    transition: opacity 1s;
+    will-change: opacity;
+
+    &.visible {
+      opacity: 1;
+    }
 
     ul {
       display: flex;
@@ -34,12 +56,43 @@
       list-style: none;
     }
 
-    li {
-      min-inline-size: 3rem;
-      font-weight: 500;
-      text-transform: uppercase;
+    a {
+      color: colors.$grey;
+      text-decoration: none;
       cursor: pointer;
-      opacity: 0.7;
+      transition: color 0.25s;
+      will-change: color;
+    }
+
+    li {
+      position: relative;
+      padding-bottom: 0.75rem;
+      overflow: hidden;
+      font-weight: bold;
+      min-inline-size: 3rem;
+
+      &::after {
+        position: absolute;
+        background-color: colors.$primary;
+        transition: scale 0.25s;
+        content: '';
+        will-change: scale;
+        inset-inline: 0;
+        inset-block-end: 0;
+        block-size: 3px;
+        scale: 0;
+      }
+
+      &:hover,
+      &:focus {
+        &::after {
+          scale: 1;
+        }
+
+        a {
+          color: colors.$white;
+        }
+      }
     }
   }
 </style>
