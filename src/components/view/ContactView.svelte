7441508178bf9091e4a76b38bf74471a0f3417c8
Dinh-Van Colomban <van.colomban@gmail.com>
2023-09-03 22:17:17 +0200

    feat(contact): ads resume fab

--- src/components/view/ContactView.svelte
+++ src/components/view/ContactView.svelte
@@ -1,6 +1,7 @@
 <script lang="ts">
   import Fab, { Icon, Label } from '@smui/fab';
   import LinkedInSvg from 'line-md/svg/linkedin.svg?component';
+  import ResumeSvg from 'line-md/svg/text-box.svg?component';
 
   import { onDestroy } from 'svelte';
   import { derived, writable } from 'svelte/store';
@@ -53,6 +54,14 @@
         </Icon>
         <Label>LinkedIn</Label>
       </Fab>
+      <Fab class={`contact-links-fab resume ${$visible$ ? 'visible' : ''}`} title="Resume" href={ExternalLink.Resume} target="_blank" extended touch>
+        <Icon>
+          {#await $delay$ then _}
+            <ResumeSvg />
+          {/await}
+        </Icon>
+        <Label>Resume</Label>
+      </Fab>
     </div>
 
     <slot />
@@ -60,6 +69,8 @@
 </Section>
 
 <style lang="scss">
+  @use 'src/theme/colors';
+
   .contact-links {
     display: flex;
     gap: 0.5rem;
@@ -67,8 +78,8 @@
 
     :global(.contact-links-fab) {
       opacity: 0;
-      transition: opacity 1s, scale 1s;
-      will-change: opacity, scale;
+      transition: opacity 1s, scale 1s, transform 1s;
+      will-change: opacity, scale, transform;
       scale: 0;
     }
 
@@ -78,11 +89,11 @@
     }
 
     :global(.visible:hover) {
-      scale: 1.1;
+      transform: scale(1.1);
     }
 
     :global(.visible:focus) {
-      scale: 1.1;
+      transform: scale(1.1);
     }
 
     :global(.github) {
@@ -91,7 +102,12 @@
 
     :global(.linkedin) {
       background-color: #0b66c2;
-      transition-delay: 0.2s;
+      transition: opacity 1s, scale 1s 0.2s, transform 1s;
+    }
+
+    :global(.resume) {
+      background-color: colors.$primary;
+      transition: opacity 1s, scale 1s 0.4s, transform 1s;
     }
   }
 </style>
--- src/data/links.data.ts
+++ src/data/links.data.ts
@@ -2,6 +2,7 @@ export enum ExternalLink {
   pages = 'https://dvcol.github.io',
   github = 'https://github.com/dvcol',
   linkedIn = 'https://linkedin.com/in/dinh-van-colomban-76b513a4',
+  Resume = 'https://dvcol.github.io/assets/pdf/Dinh-Van_Colomban_Resume_2023.pdf',
 }
 
 export enum HeaderLink {
